35dd9d22c48e37f7c8194d549c0befb649033943
max ulidtko <ulidtko@gmail.com>
2018-10-01 12:23:31 +0300

    add dump command

--- efm8loadusb.py
+++ efm8loadusb.py
@@ -255,6 +255,94 @@ def do_runapp(dev):
     hid_set_report(dev, [36, 3, 0x36, 0x00, 0x00])
     hid_get_report(dev)
 
+def cmd_dump(dev, opts):
+    """ Dumps the firmware. Slowly. Essentially bruteforcing the CRC16 feature. """
+    #-- this is a permutation of range(256) sorted by occurence frequency to optimize search
+    freq_order = (
+        0x0, 0x12, 0x90, 0x1, 0xe0, 0x2, 0xf0, 0x22, 0xa3, 0x80, 0xff, 0x3, 0x20,
+        0xe5, 0xe4, 0x75, 0xae, 0xfb, 0x7f, 0xaf, 0x82, 0xef, 0x83, 0x60, 0x5, 0xf5,
+        0x70, 0x4, 0xfe, 0xe7, 0x74, 0x7, 0xb1, 0xc, 0x24, 0x7a, 0x6, 0xd0, 0x40,
+        0x8, 0xc0, 0xfd, 0xfa, 0x79, 0x21, 0xb, 0x9, 0x7b, 0x54, 0x30, 0x94, 0xc3,
+        0xfc, 0xee, 0xd1, 0x7d, 0x28, 0x13, 0xa, 0x10, 0xc8, 0x50, 0xed, 0xd, 0xe9,
+        0xb4, 0x7e, 0x14, 0x25, 0xdf, 0xb0, 0x64, 0xf9, 0xf, 0x2c, 0x29, 0x2b, 0xc5,
+        0xec, 0x11, 0x44, 0xe, 0xeb, 0x15, 0x16, 0xf8, 0xcc, 0xd3, 0x8f, 0xaa, 0x27,
+        0x93, 0xa7, 0x43, 0xbb, 0x92, 0xa5, 0xc2, 0xab, 0xea, 0x4e, 0x9e, 0x53,
+        0xf6, 0xf2, 0x7c, 0xad, 0x17, 0xc4, 0x89, 0x33, 0x71, 0x1d, 0xa2, 0x46,
+        0x8a, 0x9a, 0x23, 0xa9, 0x8d, 0x1b, 0x1f, 0x1c, 0x8e, 0xce, 0x78, 0x3a,
+        0x2a, 0xd2, 0xa8, 0x9c, 0x26, 0x1e, 0x1a, 0xa4, 0x88, 0x65, 0xcd, 0x95,
+        0x3c, 0xb8, 0x42, 0xe3, 0x9d, 0x2f, 0x6d, 0xe1, 0x4c, 0xe2, 0x38, 0xb6,
+        0xa0, 0x91, 0xe8, 0x9f, 0xd8, 0x18, 0x4f, 0xb2, 0xac, 0xb3, 0xcf, 0x8c,
+        0xca, 0xde, 0x32, 0xc6, 0xe6, 0x85, 0xc9, 0x99, 0x4b, 0x96, 0xa1, 0x3e,
+        0x73, 0xf7, 0xf3, 0x98, 0xd5, 0x81, 0x97, 0xc1, 0x35, 0xb5, 0x31, 0xd4,
+        0xa6, 0x19, 0xf4, 0x69, 0xb9, 0xbc, 0x76, 0x55, 0x41, 0x48, 0x2d, 0xda,
+        0x45, 0x86, 0x52, 0x4a, 0x6c, 0xba, 0x6e, 0x9b, 0x37, 0xdc, 0x3b, 0x49,
+        0x61, 0xcb, 0x5e, 0x6f, 0x6b, 0x58, 0xdb, 0xf1, 0x68, 0x87, 0x2e, 0xbd,
+        0xdd, 0x47, 0x39, 0x5a, 0x56, 0xbf, 0xb7, 0x36, 0x66, 0x77, 0x5d, 0x3d,
+        0xbe, 0x34, 0x63, 0x4d, 0xd9, 0xd7, 0x5b, 0x84, 0x5f, 0x57, 0x59, 0x5c,
+        0x3f, 0x8b, 0x62, 0x72, 0x67, 0x51, 0x6a, 0xc7, 0xd6
+    )
+    empties = {size: crc16_ccitt(0, [0xff] * size) for size in (512,256,128,64,32,16,8,4,2,1)}
+    freq_crc = [(byte, crc16_ccitt(0, [byte])) for byte in freq_order]
+
+    #-- first, try to quickly localize non-empty areas
+    pending_blocks = [(a, a + 512) for a in range(0, 0x4000, 512)]
+    known_bytes = {i: None for i in range(0, 0x4000)}
+    while pending_blocks:
+        blockA, blockB = pending_blocks.pop()
+        blocksize = blockB - blockA
+        emptyCRC = empties[blocksize]
+        checkRet = do_verify(dev, blockA, blockB-1, emptyCRC)
+        if 0x40 == checkRet:
+            #-- gotcha, the whole block is filled with ones
+            for i in range(blockA, blockB):
+                known_bytes[i] = 0xff
+            continue
+        if 0x43 == checkRet:
+            #-- hmm, got some bytes!
+            split = (blockA + blockB) // 2
+            if blocksize > 1:
+                pending_blocks.append( (blockA, split) )
+                pending_blocks.append( (split, blockB) )
+            continue
+        warnings.warn(
+            "Unexpected ret 0x{:02X} from verify({:04X}, {:04X}, {:04X})"
+            .format(checkRet, blockA, blockB-1, emptyCRC)
+        )
+
+    #-- second, find missing byte values
+    total = sum(1 for v in known_bytes.values() if v is None)
+    print("== {} bytes to guess ==".format(total))
+    sofar = 0
+    for cursor in known_bytes.keys():
+        if known_bytes[cursor] is not None:
+            continue
+        for found, candidate in freq_crc:
+            checkRet = do_verify(dev, cursor, cursor, candidate)
+            if 0x43 == checkRet:
+                continue
+            if 0x40 == checkRet:
+                known_bytes[cursor] = found
+                break
+            warnings.warn(
+                "Unexpected ret 0x{:02X} from verify({:04X}, {:04X}, {:04X})"
+                .format(checkRet, cursor, cursor, candidate)
+            )
+        else:
+            warnings.warn("Couldn't find a byte value??") #-- shouldn't fire
+
+        sofar += 1
+        percent = sofar / total
+        progress = "#" * int(24 * percent) + "-" * int(24 * (1 - percent))
+        eraseline = "\x1B[2K\x1B[G"
+        print("{}[{}] {:2.1f}% done ({} of {})"
+              .format(eraseline, progress, percent * 100, sofar, total),
+              end='')
+    print('')
+
+    #-- finally, save the image
+    img = IntelHex(known_bytes)
+    img.tofile(opts.img, format='hex') # can do format='bin'
+
 def trace(direction, content):
     """ Trace device-host communications """
     if opts.trace:
@@ -265,8 +353,9 @@ def trace(direction, content):
             hex = hex.replace('24', '$', 1)
             print(hex + " -> ", end='')
     else:
-        if direction == 'in':
-            print(chr(content[0]), end='')
+        #if direction == 'in':
+        #    print(chr(content[0]), end='')
+        pass
 
 def main(): #pylint: disable=missing-docstring
     argP = argparse.ArgumentParser(
@@ -289,6 +378,13 @@ def main(): #pylint: disable=missing-docstring
     cmdFlash.add_argument('img', metavar="IHEX",
                           type=argparse.FileType('r'), default=sys.stdin,
                           help="an Intel HEX firmware file [STDIN]")
+
+    cmdDump = actP.add_parser('dump', help="Dump the firmware (SLOW) via CRC16")
+    cmdDump.set_defaults(cmd=cmd_dump)
+    cmdDump.add_argument('img', metavar="IHEX",
+                         type=argparse.FileType('w'),
+                         help="filename to save the ihex dump")
+
     global opts
     opts = argP.parse_args()
     device = locate_device(argP, opts)
